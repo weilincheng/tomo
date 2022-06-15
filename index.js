7933a4d4f2d8369db47704ac426dccd4eae3687b
weilincheng <7236196+weilincheng@users.noreply.github.com>
2022-06-15 17:43:54 +0800

    feat: add dotenv

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.port || 8080;
 const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");

76f36357dc0794ac056e35e77eb07486e8b32634
weilincheng <7236196+weilincheng@users.noreply.github.com>
2022-06-15 17:37:22 +0800

    feat: setup websocket to emit new location

--- public/google_map_prototype/script.js
+++ public/google_map_prototype/script.js
@@ -1,10 +1,59 @@
-let map;
-
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 25.03843, lng: 121.532488 }, //25.038430, 121.532488
-    zoom: 15,
+const initMap = () => {
+  const appWorksSchool = { lat: 25.03843, lng: 121.532488 };
+  const markersList = new Map();
+  const map = new google.maps.Map(document.getElementById("map"), {
+    center: appWorksSchool,
+    zoom: 18,
   });
-}
+  const infoWindow = new google.maps.InfoWindow();
 
+  const updateMarker = (id, pos) => {
+    const marker = markersList.get(id);
+    marker.setPosition(pos);
+  };
+
+  const createMarker = (id, pos) => {
+    const marker = new google.maps.Marker({
+      position: pos,
+      map: map,
+      label: {
+        color: "blue",
+        fontWeight: "bold",
+        text: id,
+      },
+    });
+    markersList.set(id, marker);
+  };
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      };
+      createMarker(socket.id, pos);
+      socket.emit("update position", { pos, id: socket.id });
+    });
+
+    navigator.geolocation.watchPosition((position) => {
+      const pos = {
+        lat: position.coords.latitude + (Math.random() - 0.5) * 0.001,
+        lng: position.coords.longitude + (Math.random() - 0.5) * 0.001,
+      };
+      updateMarker(socket.id, pos);
+      socket.emit("update position", { pos, id: socket.id });
+    });
+  }
+  socket.on("update position", (data) => {
+    const { id, pos } = data;
+    console.log(`new position ${JSON.stringify(pos)} received from ${id}`);
+    if (markersList.has(id)) {
+      updateMarker(id, pos);
+    } else {
+      createMarker(id, pos);
+    }
+  });
+};
+
+const socket = io(`https://${window.location.host}`);
 window.initMap = initMap;

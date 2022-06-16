7e71840848a089a38ac3fc8d9a8aa931958d7241
weilincheng <7236196+weilincheng@users.noreply.github.com>
2022-06-16 18:51:26 +0800

    feat: setup connection to mysql

--- server/models/mysql_connection.js
+++ server/models/mysql_connection.js
@@ -1,1 +1,16 @@
 require("dotenv").config();
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_CONNECTION_LIMIT } =
+  process.env;
+
+const mysql = require("mysql2/promise");
+const pool = mysql.createPool({
+  host: DB_HOST,
+  user: DB_USER,
+  processword: DB_PASSWORD,
+  database: DB_NAME,
+  waitForConnections: true,
+  connectionLimit: DB_CONNECTION_LIMIT,
+  queueLimit: 0,
+});
+
+module.exports = { pool };

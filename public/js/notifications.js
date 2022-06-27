b868db25089ee39027c08708788e89d10d7b0a73
weilincheng <7236196+weilincheng@users.noreply.github.com>
2022-06-27 10:55:45 +0800

    feat: render notificaitons on notifications page

--- public/js/notifications.js
+++ public/js/notifications.js
@@ -40,11 +40,59 @@ const getNotifications = async () => {
 
 const renderNotifications = async () => {
   const notifications = await getNotifications();
+  const notificationSection = $("#notifications-section");
   for (const notification of notifications) {
-    console.log("notifications", notification);
+    const {
+      profile_image: profileImage,
+      sender_nickname: nickname,
+      content,
+      id,
+      type,
+      has_read: hasRead,
+      created_at: createdAt,
+    } = notification;
+    const date = new Date(createdAt);
+    const options = { month: "long" };
+    const month = new Intl.DateTimeFormat("en-US", options).format(date);
+    const dateString = `${month} ${date.getDate()}`;
+    const notificationCard = $(
+      '<div class="row border-bottom my-2 py-2 d-flex justify-content-between align-items-center">'
+    );
+    if (!hasRead) {
+      notificationCard.css("background-color", "#79DAE8");
+    }
+    const notificationLeftColumn = $('<div class="col-1">');
+    const notificationMiddleColumn = $('<div class="col-10 pe-3">');
+    const notificationRightColumn = $('<div class="col-1">');
+    const notificationProfile = $(
+      '<div><img class="rounded-pill img-thumbnail"></div>'
+    );
+    const notificationDateConetnt = $(
+      '<p class="text-secondary my-0 px-2"></p>'
+    ).text(dateString);
+    const notificationSummary = $('<div class="fs-5"></div>');
+    if (type === "follow") {
+      notificationSummary.text(`${nickname} followed you`);
+    } else if (type === "post") {
+      notificationSummary.text(`${nickname} published a new post`);
+    }
+    const notificationContent = $('<div class="fs-6 text-secondary"></div>');
+    notificationProfile
+      .children("img")
+      .attr("src", `${cloudfrontUrl}/${profileImage}`);
+    notificationContent.text(content);
+    notificationLeftColumn.append(notificationProfile);
+    notificationMiddleColumn.append(notificationSummary);
+    notificationMiddleColumn.append(notificationContent);
+    notificationRightColumn.append(notificationDateConetnt);
+    notificationCard.append(notificationLeftColumn);
+    notificationCard.append(notificationMiddleColumn);
+    notificationCard.append(notificationRightColumn);
+    notificationSection.append(notificationCard);
   }
 };
 
 checkAccessToken();
 const currentUserId = parseInt(localStorage.getItem("userId"));
 renderNotifications(currentUserId);
+const cloudfrontUrl = "https://d3efyzwqsfoubm.cloudfront.net";
--- server/models/notifications_model.js
+++ server/models/notifications_model.js
@@ -5,11 +5,11 @@ const getNotifications = async (userId) => {
   try {
     await conn.query("START TRANSACTION");
     const [result] = await conn.query(
-      "SELECT c.type, n.created_at, c.content, u.nickname AS sender_nickname, u.id AS sender_user_id, u.profile_image FROM notifications AS n INNER JOIN notification_content as c ON n.id = c.notification_id INNER JOIN users as u ON u.id = n.sender_user_Id WHERE receiver_user_id = ?",
+      "SELECT n.has_read, c.type, n.created_at, c.content, u.nickname AS sender_nickname, u.id AS sender_user_id, u.profile_image FROM notifications AS n INNER JOIN notification_content as c ON n.id = c.notification_id INNER JOIN users as u ON u.id = n.sender_user_Id WHERE receiver_user_id = ? ORDER BY n.created_at DESC",
       [userId]
     );
     await conn.query(
-      "UPDATE `notifications` SET `read` = true WHERE receiver_user_id = ?",
+      "UPDATE notifications SET has_read = true WHERE receiver_user_id = ?",
       [userId]
     );
     await conn.query("COMMIT");

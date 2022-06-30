5e4dee78a3c7d09089beff71722fb7f5436de26e
weilincheng <7236196+weilincheng@users.noreply.github.com>
2022-06-30 16:03:43 +0800

    chore: setup AWS codeDeploy

---
+++ scripts/application_start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo chmod -R 777 /home/ec2-user/tomo
+cd /home/ec2-user/tomo
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+npm install
+node index.js > app.out.log 2> app.err.log < /dev/null &

b3e831fb813463f9dab12746d360f4ef2b3d862e
weilincheng <7236196+weilincheng@users.noreply.github.com>
2022-07-01 13:33:33 +0800

    fix: add source .bash_profile

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+source /home/ec2-user/.bash_profile
 
 
 sudo chmod -R 777 /home/ec2-user/tomo
@@ -7,7 +8,6 @@ cd /home/ec2-user/tomo
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-source /home/ec2-user/.bash_profile
 
 npm install
 #node index.js > app.out.log 2> app.err.log < /dev/null &

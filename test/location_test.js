bb5acc84fe4f86e4d8f848e86a8c18b2e197353d
weilincheng <7236196+weilincheng@users.noreply.github.com>
2022-07-07 18:49:04 +0800

    test: test zoomLevel 19

--- test/location_test.js
+++ test/location_test.js
@@ -10,12 +10,25 @@ describe("Get users' location", function () {
       const res = await requester
         .get("/api/v1/location")
         .set("Authorization", `Bearer ${accessToken}`)
-        .query({ lngLL: 121, lngUR: 122, latLL: 24, latUR: 26 });
+        .query({ latLL: 24, latUR: 26, lngLL: 121, lngUR: 122 });
       const { type, clusterSize } = res.body[0];
       should.exist(res.body);
       res.body.should.have.lengthOf(1);
       clusterSize.should.equal(10);
       type.should.equal("clusterMarker");
+    });
+
+    it("should return 10 users", async function () {
+      const signin = await requester
+        .post("/api/v1/user/signin")
+        .send({ provider: "native", email: "test@test.com", password: "test" });
+      const accessToken = signin.body.access_token;
+      const res = await requester
+        .get("/api/v1/location")
+        .set("Authorization", `Bearer ${accessToken}`)
+        .query({ latLL: 24, latUR: 26, lngLL: 121, lngUR: 122, zoomLevel: 19 });
+      should.exist(res.body);
+      res.body.should.have.lengthOf(10);
     });
   });
 });

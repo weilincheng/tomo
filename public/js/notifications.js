46b6457d87d35ef3ee131bae2df54af50235d6ae
weilincheng <7236196+weilincheng@users.noreply.github.com>
2022-07-11 17:24:31 +0800

    style: align the border style

--- public/js/notifications.js
+++ public/js/notifications.js
@@ -60,7 +60,7 @@ const renderNotifications = async () => {
     const month = new Intl.DateTimeFormat("en-US", options).format(date);
     const dateString = `${month} ${date.getDate()}`;
     const notificationCard = $(
-      '<div class="ps-4 notification-card row border-top border-start border-end py-4 d-flex justify-content-between align-items-center">'
+      '<div class="ps-4 notification-card row border-light border py-4 d-flex justify-content-between align-items-center">'
     );
     if (!hasRead) {
       notificationCard.addClass("text-bg-light");

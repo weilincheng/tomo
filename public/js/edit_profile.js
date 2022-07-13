8809531795344fcac66e466bb35b694fa424ad25
weilincheng <7236196+weilincheng@users.noreply.github.com>
2022-07-13 11:50:48 +0800

    feat: input validation

--- public/js/edit_profile.js
+++ public/js/edit_profile.js
@@ -1,19 +1,3 @@
-// const verifyToken = async (accessToken) => {
-//   const result = await fetch("/api/v1/user/profile", {
-//     method: "GET",
-//     headers: {
-//       Authorization: `Bearer ${accessToken}`,
-//     },
-//   });
-//   const resultJson = await result.json();
-//   if (resultJson.error) {
-//     alert(resultJson.error);
-//     localStorage.clear();
-//     return;
-//   }
-//   return resultJson;
-// };
-
 const getUserInfo = async (userId) => {
   const accessToken = localStorage.getItem("accessToken");
   const headers = {
